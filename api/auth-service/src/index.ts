7f84caaa6da4beb5d93db0d141803b1c923256aa
Mattéo Houillon <maatteoo.h@gmail.com>
2024-10-29 14:16:33 +0100

    add db connection

--- api/auth-service/src/index.ts
+++ api/auth-service/src/index.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
 import authRoutes from './routes/auth.routes';
+import { connectDatabase } from './controllers/database.controller';
 
 const app = express();
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 9001;
 
 app.use(express.json());
+
+connectDatabase();
 
 app.get('/', (req: Request, res: Response) => {
   res.send("Hello from auth-service");
