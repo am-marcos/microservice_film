e85891146fc2db80067111f10a06801ced5f7c07
Mattéo Houillon <maatteoo.h@gmail.com>
2024-10-29 15:20:34 +0100

    fix secrete key usage wrong type

--- api/auth-service/src/utils/token.utils.ts
+++ api/auth-service/src/utils/token.utils.ts
@@ -1,11 +1,9 @@
 import jwt from 'jsonwebtoken';
 
-const SECRET_KEY = process.env.SECRET_KEY as string;
-
 export const generateToken = (userId: string) => {
-    return jwt.sign({ userId }, SECRET_KEY, { expiresIn: '1h' });
+    return jwt.sign({ userId }, `${process.env.SECRET_KEY}`, { expiresIn: '1h' });
 };
 
 export const verifyToken = (token: string) => {
-    return jwt.verify(token, SECRET_KEY);
+    return jwt.verify(token, `${process.env.SECRET_KEY}`);
 };
