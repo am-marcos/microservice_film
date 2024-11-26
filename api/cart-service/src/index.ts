e9f29aef7d5987bdb406d4e7a20b569e0f938df8
Loicarthur <89398456+Loicarthur@users.noreply.github.com>
2024-11-26 10:37:01 +0100

    Mise à jour de package-lock.json

--- api/cart-service/src/index.ts
+++ api/cart-service/src/index.ts
@@ -1,1 +1,43 @@
-console.log('Hello from cart-service');
+import express from 'express';
+import connectDB from './config/db'
+export const app = express();
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+import cart from './controller/cart';
+
+const options = {
+    definition : {
+        openapi : "3.0.0",
+        info: {
+            title : "Documentation API",
+            version : "1.0.0",
+            description : "A simple doc API"
+        },
+        servers: [
+            {
+                url : 'http://localhost:1234'
+            }
+        ], 
+    },
+    apis: ["./controller/*.ts"]
+}
+
+connectDB()
+
+const specs = swaggerJsdoc(options)
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
+
+app.use('/cart', cart)
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on localhost:${process.env.PORT}`);
+});
+
+exports.default = app
